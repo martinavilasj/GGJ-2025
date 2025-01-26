399f7be316b6d36fe4cba175253451e84e0b8bfd
Bernardo Sanchez <103523880+Welleh@users.noreply.github.com>
2025-01-25 23:52:44 -0300

    Hamster animated

--- scripts/rigid_body_2d.gd
+++ scripts/rigid_body_2d.gd
@@ -1,5 +1,9 @@
 extends RigidBody2D
 
+enum PlayerState { IDLE, RUN, JUMP, FALL, HURT}
+
+var  _state: PlayerState = PlayerState.IDLE
+
 @export var velocidad_inicial = 20
 var velocidad = velocidad_inicial
 @export var jump_velocity_inicial = 200
@@ -9,8 +13,9 @@ var oxigeno = 100
 var disminuidor_oxigeno = 0.4
 @onready var timer_oxigeno = $Timer_Oxigeno
 @onready var barra_oxigeno = $CanvasLayer/Control/ProgressBar
+@onready var timer_ralentizar: Timer = $Timer_ralentizar
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
-@onready var timer: Timer = $Timer
 var ralentizado = false
 
 func _ready() -> void:
@@ -21,8 +26,10 @@ func _physics_process(delta: float) -> void:
 	
 	if get_colliding_bodies().size()>0 and Input.is_action_pressed("ui_right"):
 		direccion = Vector2(1 , 0)
+		animated_sprite_2d.flip_h = false
 	elif get_colliding_bodies().size()>0 and Input.is_action_pressed("ui_left"):
 		direccion = Vector2(-1 , 0)
+		animated_sprite_2d.flip_h = true
 	else:
 		direccion = Vector2(0, 0)
 		
@@ -31,19 +38,27 @@ func _physics_process(delta: float) -> void:
 		apply_central_impulse(direccion * jump_velocity)
 
 	apply_central_impulse(direccion * velocidad)
+	calculate_states()
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	barra_oxigeno.value = oxigeno
+	animated_sprite_2d.rotation = -rotation
 
 func ralentizar(tiempo,reduccion):
 	if not ralentizado:
-		timer.start(tiempo)
+		timer_ralentizar.start(tiempo)
 		linear_velocity *= reduccion
 		velocidad *= reduccion
 		jump_velocity *= reduccion
 		ralentizado = true
+
+func get_input() -> void:
+	if Input.is_action_pressed("ui_left"):
+		animated_sprite_2d.flip_h = true
+	elif Input.is_action_pressed("ui_right"):
+		animated_sprite_2d.flip_h = false
 
 
 func _on_timer_timeout() -> void:
@@ -52,4 +67,31 @@ func _on_timer_timeout() -> void:
 	ralentizado = false
 
 func _on_timer_oxigeno_timeout() -> void:
-	pass # Replace with function body.
+	if oxigeno > 0:
+		oxigeno -= disminuidor_oxigeno
+		timer_oxigeno.start()
+
+func disminuir_oxigeno(valor: float):
+	var resta = (oxigeno*valor)/100
+	oxigeno -= resta
+
+func calculate_states() -> void:
+	if Input.is_action_pressed("ui_right") or Input.is_action_pressed("ui_left") == true:
+		set_state(PlayerState.RUN)
+	else:
+		set_state(PlayerState.IDLE)
+
+func set_state(new_state: PlayerState) -> void:
+	if new_state ==_state:
+		return
+	_state = new_state
+	
+	match _state:
+		PlayerState.IDLE:
+			animated_sprite_2d.play("idle")
+		PlayerState.RUN:
+			animated_sprite_2d.play("caminar")
+		PlayerState.JUMP:
+			animated_sprite_2d.play("salto")
+		PlayerState.FALL:
+			animated_sprite_2d.play("caida")
