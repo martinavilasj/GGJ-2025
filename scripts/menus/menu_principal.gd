274be3097c614fb983e38920310b90e16bda3126
Martin Avila <martinavila.sj@gmail.com>
2025-01-26 11:58:33 -0300

    Menu principal

--- scripts/menus/menu_principal.gd
+++ scripts/menus/menu_principal.gd
@@ -1,7 +1,10 @@
 extends Control
 
 @onready var animacion: AnimationPlayer = $CanvasGroup/AnimationPlayer
+@onready var timer: Timer = $Timer
+@onready var disclaimer: TextureRect = $disclaimer
 
+@export var tiempo_disclaimer: int = 5
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,10 +17,17 @@ func _process(delta: float) -> void:
 
 
 func _on_nuevo_juego_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/nivel1.tscn")
+	start_game()
 	
 func _on_creditos_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/menus/creditos.tscn")
 
 func _on_salir_pressed() -> void:
 	get_tree().quit()
+
+func start_game() -> void:
+	timer.start(tiempo_disclaimer)
+	disclaimer.visible = true
+
+func _on_timer_timeout() -> void:
+	get_tree().change_scene_to_file("res://scenes/nivel1.tscn")
